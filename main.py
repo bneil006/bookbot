16916c1a98908d35c13951940db89fe4b7efcb41
bneil006 <brmneil2@gmail.com>
2024-03-24 20:32:42 -0500

    Completed Print a Report step on Course 3

--- main.py
+++ main.py
@@ -7,11 +7,35 @@ def count_how_many_different_characters(text):
     text = text.lower()
 
     for i in text:
-        if i in letter_dict:
-            letter_dict[i] +=1
-        else:
-            letter_dict[i] = 1
+        if i.isalpha():
+            if i in letter_dict:
+                letter_dict[i] +=1
+            else:
+                letter_dict[i] = 1
     return letter_dict
+
+def sort_on(d):
+    return d["number"]
+
+def report_on_words_and_characters(text):
+    word_count = get_word_count(text)
+    character_count = count_how_many_different_characters(text)
+    character_list = []
+
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print("")
+
+    for character in character_count:
+        character_list.append({"character": character, "number": character_count[character]})
+    character_list.sort(reverse=True, key=sort_on)
+
+    for i in character_list:
+        print(f"The '{i['character']}' character was found {i['number']} times")
+
+    print(f"--- End Report ---")
+    return
+
     
 def get_book_text(path):
     with open(path) as f:
@@ -20,8 +44,6 @@ def get_book_text(path):
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    
-    character_count = count_how_many_different_characters(text)
-    print(character_count)
+    report_on_words_and_characters(text)
     
 main()

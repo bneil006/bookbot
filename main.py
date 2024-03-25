433228d5425d0f162984307f3cdc26f9ff58665d
bneil006 <brmneil2@gmail.com>
2024-03-24 19:33:42 -0500

    Created a book reading function, character counter & word counter

---
+++ main.py
@@ -0,0 +1,29 @@
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def count_how_many_different_characters(text):
+    letter_dict = {}
+    text = text.lower()
+
+    for i in text:
+        if i in letter_dict:
+            letter_dict[i] +=1
+        else:
+            letter_dict[i] = 1
+    return letter_dict
+    
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    
+    character_count = count_how_many_different_characters(text)
+    print(character_count)
+    
+main()
